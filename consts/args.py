f34fe16152db7245d9afea57cd12a1449d747c69
chandeepadissanayake@gmail.com <Chandee1>
2019-10-20 01:27:39 +0530

    Added support to predict using a text file.

--- consts/args.py
+++ consts/args.py
@@ -3,4 +3,7 @@ KEY_PREDICT_MODEL_FOLDER_PATH = "model"
 DESC_PREDICT_MODEL_FOLDER_PATH = "Absolute path to the folder in which the model is saved."
 
 KEY_PREDICT_DATE = "date"
-DESC_PREDICT_DATE = "The date at which the exchange rate is to be predicted. (More into the future, less accurate it is."
+DESC_PREDICT_DATE = "The date at which the exchange rate is to be predicted. (More into the future, less accurate it is.)"
+
+KEY_PREDICT_DATE_FILE = "df"
+DESC_PREDICT_DATE_FILE = "A text file which would contain a single date per line each formatted in US Format(MM/dd/yyyy)"
--- predict.py
+++ predict.py
@@ -4,11 +4,20 @@ from model import Model
 
 parser = argparse.ArgumentParser(description="Predict Exchange Rates CLI")
 parser.add_argument("-" + consts_args.KEY_PREDICT_MODEL_FOLDER_PATH, required=True, help=consts_args.DESC_PREDICT_MODEL_FOLDER_PATH)
-parser.add_argument("-" + consts_args.KEY_PREDICT_DATE, required=True, help=consts_args.DESC_PREDICT_DATE)
+parser.add_argument("-" + consts_args.KEY_PREDICT_DATE, help=consts_args.DESC_PREDICT_DATE)
+parser.add_argument("-" + consts_args.KEY_PREDICT_DATE_FILE, help=consts_args.DESC_PREDICT_DATE_FILE)
 
 args = vars(parser.parse_args())
 
 model = Model(args[consts_args.KEY_PREDICT_MODEL_FOLDER_PATH])
-predicted_value = model.predict(args[consts_args.KEY_PREDICT_DATE])
+if args[consts_args.KEY_PREDICT_DATE] is not None:
+    predicted_value = model.predict(args[consts_args.KEY_PREDICT_DATE])
+    print("Predicted Exchange Rate on %s is %f" % (args[consts_args.KEY_PREDICT_DATE], predicted_value))
+elif args[consts_args.KEY_PREDICT_DATE_FILE] is not None:
+    input_file = open(args[consts_args.KEY_PREDICT_DATE_FILE], "r")
+    for date in input_file:
+        date = date.strip()
+        predicted_value_for_date = model.predict(date)
+        print("Predicted Exchange Rate on %s is %f" % (date, predicted_value_for_date))
 
-print("Predicted Exchange Rate on %s is %f" % (args[consts_args.KEY_PREDICT_DATE], predicted_value))
+    input_file.close()
